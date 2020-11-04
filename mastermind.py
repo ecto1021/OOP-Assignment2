1eeed145c978bc48eb7bb61b0431613cc4703dce
Steven Harris <harsa020@mymail.unisa.edu.au>
2020-11-04 17:29:29 +1030

    updated play method to ask questions

--- mastermind.py
+++ mastermind.py
@@ -20,29 +20,54 @@ class Mastermind:
 
     def play(self):
         players = []
-        choseGameMode = str(input('''
+
+        choice = input("""
+        What would you like to do?
+        (P)lay the game
+        (q)uit the game
+        """)
+        if choice in ["p","P"]:
+            choseGameMode = str(input('''
                 Select which game you want to play: 
                 (A) Original Mastermind for 2 Players 
                 (B) Original Mastermind for 1 Player 
                 (C) Mastermind44 for 4 Players 
                 *Enter A, B, or C to continue* 
-                '''))
-
-        if choseGameMode in ['A','a']:
-           self.name = input("Player 1 please enter your name: ")
-           self.listOfPlayers.append(self.name)
-           self.name = input("Player 2 please enter your name: ")
-           self.listOfPlayers.append(self.name)
-        elif choseGameMode in ["B" ,"b"]:
-            createName = input("Player 1 please enter your name: ")
-            self.listOfPlayers.append(self.name)
-        else:
-            print("please restart the game")
+                    '''))           
+            if choseGameMode in ['A','a']:
+                self.name = input("Player 1 please enter your name: ")
+                self.listOfPlayers.append(self.name)
+                self.name = input("Player 2 please enter your name: ")
+                self.listOfPlayers.append(self.name)
+            elif choseGameMode in ["B" ,"b"]:
+                self.name =  input("Please enter your name: ")
+                self.listOfPlayers.append(self.name)
+            elif choseGameMode in ["C" ,"c"]:
+                self.name = input("Player 1 please enter your name: ")
+                self.listOfPlayers.append(self.name)
+                self.name = input("Player 2 please enter your name: ")
+                self.listOfPlayers.append(self.name)
+                self.name = input("Player 3 please enter your name: ")
+                self.listOfPlayers.append(self.name)
+                self.name = input("Player 4 please enter your name: ")
+                self.listOfPlayers.append(self.name) 
+            else:
+                print("please restart the game")
+                quit()
+        elif choice in ['Q','q']:
             quit()
-            
+    
 
     def getPlayerName(self):
-        return self.name
+      return self.listOfPlayers
+
+
+
+
+
+    def showList(self):
+        for i in self.listOfPlayers:
+            print(i)
 
 
 
@@ -57,4 +82,3 @@ m = Mastermind()
 
 m.play()
 print(m.getPlayerName())
-print(m.listOfPlayers)
