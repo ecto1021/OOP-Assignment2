3d4f085087d11dc4697515f8a8a57c5f7b897317
Steven Harris <harsa020@mymail.unisa.edu.au>
2020-11-07 18:34:16 +1030

    Updated Play class

--- mastermind.py
+++ mastermind.py
@@ -50,65 +50,70 @@ class Mastermind:
         Developed by Steven Harris
         COMP 1046 Object-Oriented Programming 
         Press Enter to Continue ''')
+    
+    def setGameMode(self):
+        '''i used a while loop here to get the function to repeat if a '''
+        playing = True
+        while playing == True:
+            choseGameMode = str(input('''
+Select which game you want to play:
+(A) Original Mastermind for 2 Players 
+(B) Original Mastermind for 1 Player 
+(C) Mastermind44 for 4 Players 
+*Enter A, B, or C to continue* '''))    
+            if  choseGameMode  in ['A','a']:
+                player1Name = input("Player 1 please enter your name: ")
+                player2Name = input("Player 2 please enter your name: ")
+                player1 = Player(player1Name)
+                player2 = Player(player2Name)
+                self.listOfPlayers.append(player1.name)
+                self.listOfPlayers.append(player2.name)
+                break
+            elif  choseGameMode  in ["B" ,"b"]:
+                player1 = Player(input("Player 1 please enter your name: "))
+                self.listOfPlayers.append(player1.name)
+                break
+            elif  choseGameMode  in ["C" ,"c"]:
+                player1Name = input("Player 1 please enter your name: ")
+                player2Name = input("Player 2 please enter your name: ")
+                player3Name = input("Player 3 please enter your name: ")
+                player4Name = input("Player 4 please enter your name: ")
+                player1 = Player(player1Name)
+                player2 = Player(player2Name)
+                player3 = Player(player3Name)
+                player4 = Player(player4Name)
+                self.listOfPlayers.append(player1.name)
+                self.listOfPlayers.append(player2.name)
+                self.listOfPlayers.append(player3.name)
+                self.listOfPlayers.append(player4.name)
+                break 
+            else:
+                print("INVALID INPUT!")
+                print('Please choose from above option')
+                continue
 
     def play(self):
-        """the main class to run, specified in assignment 2
-        i used a while loop here to get the function to repeat if a 
-        """
+
+        """the main class to run, specified in assignment 2"""
         playing = True
         while playing == True:
             choice = input("""
-            What would you like to do?
-            (P)lay the game
-            (q)uit the game
-            """)
-            if choice in ["p","P"]:
-                choseGameMode = str(input('''
-                    Select which game you want to play: 
-                    (A) Original Mastermind for 2 Players 
-                    (B) Original Mastermind for 1 Player 
-                    (C) Mastermind44 for 4 Players 
-                    *Enter A, B, or C to continue* 
-                        '''))    
-                if  choseGameMode  in ['A','a']:
-                    player1Name = input("Player 1 please enter your name: ")
-                    player2Name = input("Player 2 please enter your name: ")
-                    player1 = Player(player1Name)
-                    player2 = Player(player2Name)
-                    self.listOfPlayers.append(player1.name)
-                    self.listOfPlayers.append(player2.name)
-                    break
-                elif  choseGameMode  in ["B" ,"b"]:
-                    player1 = Player(input("Player 1 please enter your name: "))
-                    self.listOfPlayers.append(player1.name)
-                    break
-                elif  choseGameMode  in ["C" ,"c"]:
-                    player1Name = input("Player 1 please enter your name: ")
-                    player2Name = input("Player 2 please enter your name: ")
-                    player3Name = input("Player 3 please enter your name: ")
-                    player4Name = input("Player 4 please enter your name: ")
-                    player1 = Player(player1Name)
-                    player2 = Player(player2Name)
-                    player3 = Player(player3Name)
-                    player4 = Player(player4Name)
-                    self.listOfPlayers.append(player1.name)
-                    self.listOfPlayers.append(player2.name)
-                    self.listOfPlayers.append(player3.name)
-                    self.listOfPlayers.append(player4.name)
-                    break 
-                else:
-                    print("INVALID INPUT!")
-                    print('Please choose from above option')
-                    continue
-
-            elif choice in ['Q','q']:
-                print("You Have Chosen to Exit")
+What would you like to do ?
+(P)lay
+(Q)uit
+""")
+            if choice in ["p", 'p']:
+                    self.setGameMode()
+                    playAgain = input("do you want to continue")
+                    if playAgain in ['n','N','no','NO','No']:
+                        playing == False
+            elif choice in['Q', "q"]:
+                print("thanks for playing!")
                 quit()
-                playing == False
-            else:
-                print('INVALID INPUT!')
-                print('Please choose from above option')
-                continue
+            break
+        if playing == False:
+           playAgain = input("do you want to continue")
+
 
     def getPlayerName(self,name):
         '''Function to return player name
@@ -130,10 +135,21 @@ class Mastermind:
 
 
 
+class originalMasterMind(Mastermind):
+    withSupport = bool
+    playerAttempts = 0
+    numberOfPlayers = 0
+    def __init__(self):
+        super()
 
 
-m = Mastermind()
-m.play()
+
+
+m2 = originalMasterMind()
+m2.play()
+
+
+
 
 
 
